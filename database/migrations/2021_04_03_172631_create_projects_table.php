384489c490e6d79412a8bd6e9c0c9057b4a802ce
rivalix <raul.alva1926@gmail.com>
2021-04-03 14:35:11 -0600

    Disenando la Base de Datos

---
+++ database/migrations/2021_04_03_172631_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('manager_id');
+            $table->timestamps();
+
+            $table->foreign('manager_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
